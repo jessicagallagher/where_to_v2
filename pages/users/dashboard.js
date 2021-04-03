f45692953b8890fe52189f0ff6be1400f52bb67a
jessicagallagher <jessicalyngallagher@me.com>
2021-04-02 23:36:22 -0400

    styling

--- pages/users/dashboard.js
+++ pages/users/dashboard.js
@@ -7,6 +7,7 @@ export default function Dashboard() {
         <title>Your Dashboard</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      {/* begin mobile / responsive sidebar */}
       <div className='h-screen flex overflow-hidden bg-white'>
         <div className='fixed inset-0 flex z-40 lg:hidden' role='dialog' aria-modal='true'>
           <div className='fixed inset-0 bg-gray-600 bg-opacity-75' aria-hidden='true'></div>
@@ -22,13 +23,14 @@ export default function Dashboard() {
 
             <div className='flex-1 h-0 pt-5 pb-4 overflow-y-auto'>
               <div className='flex-shrink-0 flex items-center px-4'>
-                <img className='h-8 w-auto' src='https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-900-text.svg' alt='Workflow'></img>
+                <img className='h-8 w-auto' src='../logo.png' alt='logo'></img>
+                <h1 className='font-accent text-dkGrey-100 inline md:text-4xl mx-auto'>Where To?</h1>
 
               </div>
               <nav className='mt-5' aria-label='Sidebar'>
                 <div className='px-2 space-y-1'>
-                  <a href='#' className='bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md'>
-                    <svg className='text-gray-500 mr-4 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
+                  <a href='#' className='bg-notWhite-100 text-dkGrey-100 group flex items-center px-2 py-2 text-base font-medium rounded-md'>
+                    <svg className='text-teal-100 mr-4 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
                 <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' />
               </svg>
               Dashboard
@@ -36,15 +38,15 @@ export default function Dashboard() {
                   </a>
                   <a href='#' className='text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md'>
               <svg className='text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
               </svg>
-              Calendar
+              Create New Itinerary
             </a>
             <a href='#' className='text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md'>
               <svg className='text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z' />
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' />
               </svg>
-              Teams
+              Calendar
             </a>
             <a href='#' className='text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md'>
               <svg className='text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
@@ -90,20 +92,29 @@ export default function Dashboard() {
           <div className='flex-shrink-0 w-14' aria-hidden='true'>
           </div>
         </div>
+        {/* begin static */}
         <div className='hidden lg:flex lg:flex-shrink-0'>
     <div className='flex flex-col w-64'>
-      <div className='flex flex-col h-0 flex-1 border-r border-gray-200 bg-gray-100'>
+      <div className='flex flex-col h-0 flex-1 border-r border-ltGrey-100 bg-ltLime-100'>
         <div className='flex-1 flex flex-col pt-5 pb-4 overflow-y-auto'>
           <div className='flex items-center flex-shrink-0 px-4'>
-            <img className='h-8 w-auto' src='https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-900-text.svg' alt='Workflow' />
+            <img className='h-8 w-auto' src='../logo.png' alt='logo' />
+            <h1 className='font-accent text-dkGrey-100 inline md:text-4xl mx-auto'>Where To?</h1>
           </div>
           <nav className='mt-5 flex-1' aria-label='Sidebar'>
             <div className='px-2 space-y-1'>
-              <a href='#' className='bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md'>
-                <svg className='text-gray-500 mr-3 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
+              <a href='#' className='hover:bg-notWhite-100 text-dkGrey-100 group flex items-center px-2 py-2 text-sm font-medium rounded-md'>
+                <svg className='text-teal-100 mr-3 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' />
                 </svg>
                 Dashboard
+              </a>
+
+              <a href='#' className='bg-notWhite-100 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md'>
+                <svg className='text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                </svg>
+                Create New Itinerary
               </a>
 
               <a href='#' className='text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md'>
@@ -111,13 +122,6 @@ export default function Dashboard() {
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' />
                 </svg>
                 Calendar
-              </a>
-
-              <a href='#' className='text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md'>
-                <svg className='text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
-                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z' />
-                </svg>
-                Teams
               </a>
 
               <a href='#' className='text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md'>
