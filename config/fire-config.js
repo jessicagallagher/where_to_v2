245e4ed023511290a4a1b49abefb57f45f8b6b5b
jessicagallagher <jessicalyngallagher@me.com>
2021-04-08 14:55:45 -0400

    trying env

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB-xEPETXSfjboKe5H0kPUu-ZdRDGfszmA',
-  authDomain: "where-to-jess.firebaseapp.com",
-  databaseURL: 'https://where-to-jess-default-rtdb.firebaseio.com/',
-  projectId: "where-to-jess",
-  storageBucket: "where-to-jess.appspot.com",
-  messagingSenderId: "914509599583",
-  appId: "1:914509599583:web:80cdf3e4090417b0f35cea"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 try {
   firebase.initializeApp(firebaseConfig);
