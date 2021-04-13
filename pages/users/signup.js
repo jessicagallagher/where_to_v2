12eecd6c691dc8652f4acc14290cda5216c997a4
jessicagallagher <jessicalyngallagher@me.com>
2021-04-12 22:24:01 -0400

    revert

--- pages/users/signup.js
+++ pages/users/signup.js
@@ -35,7 +35,6 @@ export default function Signup() {
   };
 
 
-
   return (
     <div className='container flex flex-col justify-center items-center mx-auto py-0 px-2 min-h-screen sm:px-6 lg:px-8'>
       <Head>
