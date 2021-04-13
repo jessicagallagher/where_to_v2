b311fcd6be648f4422ee9f0f06e39ea3de480d97
jessicagallagher <jessicalyngallagher@me.com>
2021-04-12 22:58:40 -0400

    working again

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,4 +1,8 @@
-import firebase from 'firebase';
+// import * as firebase from "firebase/app";
+// import firebase from 'firebase';
+import firebase from 'firebase/app';
+import "firebase/auth";
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
