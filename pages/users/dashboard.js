d3918aa0ecd09de3d9759ad0626f4c292f8d2eed
jessicagallagher <jessicalyngallagher@me.com>
2021-04-13 16:19:03 -0400

    trying to get responsive sidebar to close

--- pages/users/dashboard.js
+++ pages/users/dashboard.js
@@ -8,6 +8,7 @@ export default function Dashboard() {
   const router = useRouter();
   const [itineraries, setItineraries] = useState([]);
   const [loggedIn, setLoggedIn] = useState(true);
+  const [open, setOpen] = useState(true);
 
   useEffect(() => {
     const unsubscribe = 
@@ -45,6 +46,10 @@ export default function Dashboard() {
     router.push('/')
   };
 
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   
 
   return (
@@ -57,9 +62,12 @@ export default function Dashboard() {
       <div className='h-screen flex overflow-hidden bg-white'>
         <div className='fixed inset-0 flex z-40 lg:hidden' role='dialog' aria-modal='true'>
           <div className='fixed inset-0 bg-gray-600 bg-opacity-75' aria-hidden='true'></div>
-          <div className='relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none'>
+          <div className={`relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none
+          transform ${
+            open ? 'translate-x-0 ease-out transition-medium' : '-translate-x-full ease-in transition-medium'
+          }`}>
             <div className='absolute top-0 right-0 -mr-12 pt-2'>
-              <button type='button' className='ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+              <button type='button' className='ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white' onClick={handleClick}>
                 <span className='sr-only'>Close sidebar</span>
                 <svg className='h-6 w-6 text-white' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
